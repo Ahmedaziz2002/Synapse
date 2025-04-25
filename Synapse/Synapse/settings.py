50d3fcc5e0e6bc7685b81ded7766007f6ae9768b
Mathewsq369 <mathewsq369@gmail.com>
2025-04-25 13:27:23 +0300

    changes

--- Synapse/Synapse/settings.py
+++ Synapse/Synapse/settings.py
@@ -139,9 +139,5 @@ LOGOUT_REDIRECT_URL="/login/"
 
 
 MEDIA_URL = '/accounts/media/'
-<<<<<<< HEAD
-MEDIA_ROOT = os.path.join(BASE_DIR, 'accounts/media')
-=======
 MEDIA_ROOT = os.path.join(BASE_DIR, 'accounts/media')
 
->>>>>>> ab35545911ccc6b54daf9f3de022e1c93a04b2c2
